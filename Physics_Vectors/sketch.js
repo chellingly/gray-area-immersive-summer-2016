0cdcc4aa54a758941dc2089b87c284d6b99018f5
Chelley Sherman <chelleysherman@gmail.com>
2016-06-21 19:32:29 -0700

    In

---
+++ Physics_Vectors/sketch.js
@@ -0,0 +1,51 @@
+var mover;
+
+function setup() {
+  createCanvas(640,360);
+  mover = new Mover(); 
+}
+
+function draw() {
+  background(51);
+  
+  mover.update();
+  // mover.checkEdges();
+  mover.display(); 
+}
+
+function Mover() {
+  this.position = createVector(width/2,height/2); //x y position
+  this.velocity = createVector();//speed in x direction, speec in y direction
+  this.acceleration = createVector(-0.001, .01);//acceleration in x direction, acceleration in y direction
+  // this.topspeed = 10;
+
+  this.update = function() {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+  };
+
+  this.display = function() {
+    stroke(0);
+    strokeWeight(2);
+    fill(127);
+    ellipse(this.position.x, this.position.y, 48, 48);
+  };
+
+  // this.checkEdges = function() {
+
+  //   if (this.position.x > width) {
+  //     this.position.x = 0;
+  //   }
+  //   else if (this.position.x < 0) {
+  //     this.position.x = width;
+  //   }
+
+  //   if (this.position.y > height) {
+  //     this.position.y = 0;
+  //   }
+  //   else if (this.position.y < 0) {
+  //     this.position.y = height;
+  //   }
+  // };
+
+}
